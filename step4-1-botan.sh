b45cb5f1a87107c1accae532203a0dfe72ba3fe3
homdx <37062532+homdx@users.noreply.github.com>
2019-02-16 17:39:54 +0300

    Update step4-1-botan.sh

--- step4-1-botan.sh
+++ step4-1-botan.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+if [ $TRAVIS ]
+then
+echo "Travis run"
 python3 --version
 export PATH=/usr/bin:/usr/local/bin:$PATH
 python3 --version
 cd $TRAVIS_BUILD_DIR
+tar -xf 2.9.0.tar.gz 
 cd botan-2.9.0
+else
+echo "No travis runing"
+fi
+
 ls -la
 python3 configure.py --prefix=/usr/local --cc=gcc --link-method=hardlink  --with-cmake --os=cygwin
 cd build
